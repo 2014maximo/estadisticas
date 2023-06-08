fddf4b87501efc92e8624872b74a63d262cb6440
2014maximo <alexuriel.777@gmail.com>
2023-06-08 01:02:34 -0500

    construyendo add data a firebase actual

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,5 +6,5 @@ import { AstroSolComponent } from './components/astro-sol/astro-sol.component';
 export const ROUTES: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home'},
     { path: 'home', component: HomeComponent },
-    { path: 'astrosol', component: AstroSolComponent }
+    { path: 'astro-sol', component: AstroSolComponent }
 ]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { EstadisticasService } from 'src/app/services/estadisticas.service';
 
-import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Firestore, collectionData, collection, addDoc, CollectionReference } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
@@ -13,23 +13,41 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class HomeComponent {
 
   formIngreso: FormGroup;
+  formGrupos: FormGroup;
+  mostrarLogin: boolean;
 
   item$: Observable<any[]>;
+
   constructor(private firestore: Firestore, private fb: FormBuilder) {
+
     this.formIngreso = this.crearFormulario();
+    this.formGrupos = this.crearFormularioGrupos();
     const collections = collection(this.firestore, 'todos-los-grupos');
+    let reference: CollectionReference<any>
+    
+    let data = {
+      "uno": 4,
+      "cuatro": 7,
+      "dos": 5,
+      "sorteo": 1233,
+      "signo": 12,
+      "fecha": "2023-03-02",
+      "tres": 7
+  }
 
     this.item$ = collectionData(collections);
+
+    this.mostrarLogin = true;
   }
 
     ngOnInit(): void {
-
       this.item$.subscribe({
         next:(resp)=>{
           console.log(resp)
         }
       })
     //this.estadisticaService.getData('todos-los-grupos');
+
   }
 
   private crearFormulario():FormGroup{
@@ -38,9 +56,21 @@ export class HomeComponent {
       password: ['', [Validators.required]]
     });
   }
+  private crearFormularioGrupos():FormGroup{
+    return this.fb.group({
+      grupos:['', [Validators.required]]
+    });
+  }
 
   enviar(){
-    
+    if(this.formIngreso.value.email === 'alexuriel.777@gmail.com' &&
+       this.formIngreso.value.password === 'Desarrollo2023$'){
+        this.mostrarLogin = false;
+    }
+  }
+
+  enviarGrupos(){
+
   }
 
 }
