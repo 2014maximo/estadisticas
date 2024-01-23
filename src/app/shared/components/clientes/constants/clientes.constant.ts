fa489cf7bab115e8dc67afe44867315cb920d308
2014maximo <alexuriel.777@gmail.com>
2024-01-23 11:39:36 -0500

    cargando clientes

--- src/app/shared/components/clientes/constants/clientes.constant.ts
+++ src/app/shared/components/clientes/constants/clientes.constant.ts
@@ -12,7 +12,7 @@ export const CLIENTES: ClienteModel[] = [
         categoriaSector: 'Lejos-medio',
         ciudad: 'Medellín',
         departamento: 'Antioquia',
-        maps:''
+        maps:'https://maps.app.goo.gl/mBeRUwr7cNRC8cT39'
     },
     {
         id:2,
@@ -24,14 +24,14 @@ export const CLIENTES: ClienteModel[] = [
         categoriaSector:'Lejos-medio',
         ciudad: 'Medellín',
         departamento: 'Antioquia',
-        maps:''
+        maps:'https://maps.app.goo.gl/Si3p9Gjen4R7fA7W9'
     },
     {
         id:3,
         nombre:'Luisa Fernanda coronel',
         departamento:'Norte de Santander',
         ciudad:'Ocaña',
-        direccion: 'Calle 15 10A88',
+        direccion: 'Calle 15 10a 88',
         referenciaUbicacion: '',
         telefono: '3134429501',
         categoriaSector:'',
@@ -44,23 +44,23 @@ export const CLIENTES: ClienteModel[] = [
         ciudad: 'Envigado',
         departamento: 'Antioquia',
         barrio: 'Barrio Obrero',
-        direccion: 'Transversal 34 c sur # 33-15',
+        direccion: 'Transversal 34c sur # 33-15',
         referenciaUbicacion: 'Primer piso - El local se llama uñas mágicas',
         telefono: '3008828621',
         categoriaSector:'Lejos-normal',
-        maps:''
+        maps:'https://maps.app.goo.gl/E2yZq7RWqLJAuKr6A'
     },
     {
         id:5,
         nombre: 'Slendy',
         barrio: 'San Javier',
-        direccion: 'Cl 49 # 102BB 35',
+        direccion: 'Calle 49 # 102bb 35',
         categoriaSector:'',
         telefono:'3042890062',
         ciudad: 'Medellín',
         departamento:'Antioquia',
-        maps:'',
-        referenciaUbicacion:''
+        maps:'https://maps.app.goo.gl/tGn4oDLyrdGtAPo18',
+        referenciaUbicacion:'El Socorro'
     },
     {
         id:6,
@@ -69,8 +69,8 @@ export const CLIENTES: ClienteModel[] = [
         categoriaSector:'',
         ciudad:'Medellín',
         departamento:'Antioquia',
-        direccion:'Cr 35a #99-56',
-        maps:'',
+        direccion:'Carrera 35a # 99-56',
+        maps:'https://maps.app.goo.gl/NVAjogztKxkwFA2F7',
         referenciaUbicacion:'',
         telefono:'3016056759'
     },
@@ -81,10 +81,130 @@ export const CLIENTES: ClienteModel[] = [
         categoriaSector:'',
         ciudad:'Medellín',
         departamento:'Antioquia',
-        direccion:'Cr 28A # 38f 33',
+        direccion:'Carrera 28a # 38f 33',
         maps:'https://maps.app.goo.gl/x1UbTyyRf7WJfWbcA',
         referenciaUbicacion:'',
         telefono: '3024017191'
+    },
+    {
+        id:8,
+        nombre:'Paula Meza',
+        barrio:'',
+        categoriaSector:'',
+        ciudad:'Girón',
+        departamento:'Santander',
+        direccion:'',
+        maps:'',
+        referenciaUbicacion:'puerto Madero Giron',
+        telefono:'3125416310'
+    },
+    {
+        id:9,
+        nombre:'Sofia',
+        barrio:'Aranjuez',
+        categoriaSector:'',
+        ciudad:'Medellín',
+        departamento:'Antioquia',
+        direccion:'Carrera 51 #95-58 un 4 piso',
+        referenciaUbicacion:'4 piso',
+        maps:'https://maps.app.goo.gl/zQfGmUrmScjGCHZBA',
+        telefono:'3022382467'
+    },
+    {
+        id:10,
+        nombre:'Adela Montoya',
+        barrio:'Robledo Kennedy',
+        categoriaSector:'',
+        ciudad:'Medellín',
+        departamento:'Antioquia',
+        direccion:'Carrera 75a #90-05',
+        maps:'https://maps.app.goo.gl/ZxyrPUFU1AEsXNuRA',
+        referenciaUbicacion:'',
+        telefono:'3136834739'
+    },
+    {
+        id:11,
+        nombre:'Mary Quiñones',
+        barrio:'Santander',
+        categoriaSector:'',
+        ciudad:'Medellín',
+        departamento:'Antioquia',
+        direccion:'Carrera 77e # 114-35',
+        maps:'https://maps.app.goo.gl/MvYdL1U1X2RWXHeD6',
+        referenciaUbicacion:'',
+        telefono:'3004935583'
+    },
+    {
+        id:12,
+        nombre:'Carolina Galeano',
+        barrio:'Belén la Nubia',
+        categoriaSector:'',
+        ciudad:'Medellín',
+        departamento:'Antioquia',
+        direccion:'Carrera 81 # 18b-23',
+        maps:'https://maps.app.goo.gl/ynFA71SPmjDTba1V9',
+        referenciaUbicacion:'Una cuadra arriba de migración colombia',
+        telefono:'3114294833'
+    },
+    {
+        id:13,
+        nombre:'Nasa Roman',
+        barrio:'Urbanización Mantia',
+        categoriaSector:'',
+        ciudad:'Itagüí',
+        departamento:'Antioquia',
+        direccion: 'Calle 14 sur # 58-60 apto 1409 torre 1',
+        maps:'https://maps.app.goo.gl/e9M9v3DDu61ZJwaC7',
+        referenciaUbicacion:'',
+        telefono:'3206581210'
+    },
+    {
+        id:14,
+        nombre:'Ana Maria',
+        barrio:'Quintas de la Quinta',
+        categoriaSector:'',
+        ciudad:'La Estrella',
+        departamento:'Antioquia',
+        direccion:'Carrera 59#82 sur 88',
+        maps:'https://maps.app.goo.gl/8kd14jk4ctSme8SJ8',
+        referenciaUbicacion:'',
+        telefono:'3043765786'
+    },
+    {
+        id:15,
+        nombre:'Lorena',
+        barrio:'Manrique',
+        categoriaSector:'',
+        ciudad:'Medellín',
+        departamento:'Antioquia',
+        direccion:'Calle 84 # 39-56',
+        maps:'https://maps.app.goo.gl/htp2X4kYexiUuXt59',
+        referenciaUbicacion:'',
+        telefono:'3023982018'
+    },
+    {
+        id:16,
+        nombre:'Yassiry Gonzalez',
+        barrio:'Enciso',
+        categoriaSector:'',
+        ciudad:'Medellín',
+        departamento:'Antioquia',
+        direccion:'Carrera 28A # 57d 41',
+        maps:'https://maps.app.goo.gl/gSMgxLkRN9EoDbok7',
+        referenciaUbicacion:'Al lado de la Y. al frente de la julia Agudelo',
+        telefono:'3013542944'
+    },
+    {
+        id:17,
+        nombre:'Nataly Martinez',
+        barrio:'Gratamira',
+        categoriaSector:'',
+        ciudad:'Medellín',
+        departamento:'Antioquia',
+        direccion:'Calle 108 # 66-27',
+        maps:'https://maps.app.goo.gl/i97Are6VWKgcz4TU6',
+        referenciaUbicacion:'',
+        telefono:'3146264292'
     }
 ]
 
@@ -108,6 +228,42 @@ export const SECTOR: ReferenciaModel[]=[
     {
         id:5,
         nombre: 'La Milagrosa'
+    },
+    {
+        id:6,
+        nombre:'Barrio Obrero'
+    },
+    {
+        id:7,
+        nombre:'Aranjuez'
+    },
+    {
+        id:8,
+        nombre:'Robledo Kennedy'
+    },
+    {
+        id:9,
+        nombre:'Santander'
+    },
+    {
+        id:10,
+        nombre:'Belén la Nubia'
+    },
+    {
+        id:11,
+        nombre:'Quintas de la Quinta'
+    },
+    {
+        id:12,
+        nombre:'Manrique'
+    },
+    {
+        id:13,
+        nombre:'Enciso'
+    },
+    {
+        id:14,
+        nombre:'Gratamira'
     }
 ]
 
@@ -119,5 +275,9 @@ export const CATEGORIA_SECTOR: ReferenciaModel[]=[
     {
         id:2,
         nombre:'Lejos-normal'
+    },
+    {
+        id:3,
+        nombre:'Arriba-no-peligroso'
     }
 ]
